8aa0ee36285a4677be4538174c4b20d9d1286489
Nitzan Goldfeder <nitzang@elementor.com>
2024-10-15 12:25:04 +0300

    welcome 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { Command, CommandRunner } from 'nest-commander';
 
+
+@Command({
+  name: 'hello',
+  arguments: '<name>'
+})
+class Greeter extends CommandRunner {
+  async run(args: string[]){
+    console.log(`Hello ${args}`)
+  }
+
+}
 
 @Module({
+  providers: [Greeter]
 })
 export class AppModule {}
