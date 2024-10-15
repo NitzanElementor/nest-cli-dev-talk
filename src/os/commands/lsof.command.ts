97ad412f2a8d196535bc7be2210f9fdaa6e8745c
Nitzan Goldfeder <nitzang@elementor.com>
2024-10-15 10:59:03 +0300

    with args

--- src/os/commands/lsof.command.ts
+++ src/os/commands/lsof.command.ts
@@ -1,10 +1,30 @@
 import { spawnSync } from "child_process";
-import { Command, CommandRunner } from "nest-commander";
+import { Command, CommandRunner, Option} from "nest-commander";
 
-@Command({ name: 'lsof' })
+type CommandOptions = {
+    port: string;
+}
+
+@Command({ 
+    name: 'lsof'
+ })
 export class LSOFCommand extends CommandRunner {
-  async run() {
-    const output = spawnSync('lsof', { shell: '/bin/bash' })
+  async run(args: string[], options: CommandOptions) {
+    console.log('args', args)
+    console.log('options', options)
+    const inputs = [];
+    if (options.port) {
+        inputs.push('-i', `:${options.port}`)
+    }
+    const output = spawnSync('lsof', inputs, { shell: '/bin/bash' })
     console.log(output.stdout.toString())
+  }
+
+  @Option({
+    description: 'Specify which port to check',
+    flags: "-p --port <port>"
+  })
+  getPort(val: string) {
+    return val;
   }
 }
